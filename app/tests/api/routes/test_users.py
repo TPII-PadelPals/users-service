32ea093527c6fbb95b2e4095b573081e259e04aa
dima1997 <franki.dimaria@gmail.com>
2024-12-13 14:43:44 -0300

    test: SCRUM-95: GET User NotAuth

--- app/tests/api/routes/test_users.py
+++ app/tests/api/routes/test_users.py
@@ -1,3 +1,4 @@
+from fastapi import status
 from httpx import AsyncClient
 
 from app.core.config import settings
@@ -37,3 +38,22 @@ async def test_read_user(
     assert content["name"] == data["name"]
     assert content["email"] == data["email"]
     assert content["phone"] == data["phone"]
+
+
+async def test_read_user_not_authorized(
+    async_client: AsyncClient, x_api_key_header: dict[str, str]
+) -> None:
+    data = {"name": "Name Surname", "email": "name@domain.com", "phone": "11 1234 1234"}
+    response = await async_client.post(
+        f"{settings.API_V1_STR}/users/", headers=x_api_key_header, json=data
+    )
+    content = response.json()
+    user_id = content["id"]
+
+    response = await async_client.get(
+        f"{settings.API_V1_STR}/users/{user_id}",
+        headers={"x-api-key": "wrong-key"},
+    )
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED
+    content = response.json()
+    assert content["detail"] == "Not Authorized"
