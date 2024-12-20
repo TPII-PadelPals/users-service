3c66d7555f5cb12ff8259f541d73df02e488df57
dima1997 <franki.dimaria@gmail.com>
2024-12-19 23:51:30 -0300

    fix: split router intro open and token; add SessionMiddleware

--- app/api/main.py
+++ app/api/main.py
@@ -1,11 +1,16 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 
 from app.api.routes import google, items, items_service, users
+from app.utilities.dependencies import get_token_header
 
-api_router = APIRouter()
-api_router.include_router(google.router, prefix="/google", tags=["google"])
-api_router.include_router(users.router, prefix="/users", tags=["users"])
-api_router.include_router(items.router, prefix="/items", tags=["items"])
-api_router.include_router(
+api_router_open = APIRouter()
+api_router_open.include_router(google.router, prefix="/google", tags=["google"])
+
+api_router_token = APIRouter(
+    dependencies=[Depends(get_token_header)],
+)
+api_router_token.include_router(users.router, prefix="/users", tags=["users"])
+api_router_token.include_router(items.router, prefix="/items", tags=["items"])
+api_router_token.include_router(
     items_service.router, prefix="/items-service", tags=["items-service"]
 )
