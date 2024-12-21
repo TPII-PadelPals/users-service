e3dd2af03a083e174f79b75c330bd4c1ecce3d93
amaruDuran <amaruduran@gmail.com>
2024-12-21 14:10:09 -0300

    test: unit test for email without at

--- app/tests/models/test_user.py
+++ app/tests/models/test_user.py
@@ -1,7 +1,7 @@
 import pytest
 
 from app.models.user import UserCreate
-
+from app.utilities.exceptions import InvalidEmailException
 
 async def test_create_user_name_min_size_is_1() -> None:
     with pytest.raises(ValueError):
@@ -13,3 +13,10 @@ async def test_create_user_name_max_length_is_255() -> None:
     with pytest.raises(ValueError):
         name = "a" * 256
         UserCreate(name=name, email="name@domain.com", phone="11 1234 5678")
+
+
+async def test_create_user_without_at_symbol_raises_error() -> None:
+    with pytest.raises(InvalidEmailException) as e:
+        UserCreate(name="Robert", email="name_without_at.com", phone="11 1234 5678")
+    
+    assert str(e.value) == "Invalid email format."
